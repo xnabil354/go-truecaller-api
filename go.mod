ec5f795bbb07973401e26873fd88edc026caf797
nabil354 <nabil.voel354@gmail.com>
2023-04-15 13:34:20 +0700

    Add Styling

--- go.mod
+++ go.mod
@@ -4,12 +4,25 @@ go 1.19
 
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.6 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/go-openapi/errors v0.20.3 // indirect
+	github.com/go-openapi/strfmt v0.21.7 // indirect
+	github.com/gookit/color v1.5.3 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible // indirect
+	github.com/jedib0t/go-pretty/v6 v6.4.6 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
-	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	go.mongodb.org/mongo-driver v1.11.3 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
+	"github.com/jedib0t/go-pretty/table"
 )
 
 type TrueCaller struct {
@@ -14,70 +16,63 @@ type TrueCaller struct {
 	countryCode string
 }
 
-type Phone struct {
-	Carrier        string `json:"carrier"`
-	NationalFormat string `json:"nationalFormat"`
+func (tc *TrueCaller) Start() {
+	color.New(color.BgHiYellow, color.FgBlack).Println(" TRUECALLER|API ")
+	fmt.Println(color.HiBlueString("======================================"))
+	fmt.Println(color.YellowString("Creator : xzhndvs"))
+	fmt.Println(color.YellowString("Instagram : @xzhndvs"))
+	fmt.Println(color.YellowString("WhatsApp : 081281524356"))
+	fmt.Println(color.HiBlueString("======================================"))
 }
 
-type ResponseData struct {
-	Name   string  `json:"name"`
-	Phones []Phone `json:"phones"`
-}
-
-type Response struct {
-	Data struct {
-		Data []ResponseData `json:"data"`
-	} `json:"data"`
-}
-
-func (tc *TrueCaller) fetchData() (*ResponseData, error) {
-	resp, err := http.Get(fmt.Sprintf("https://xzhndvs.vercel.app/api/truecaller?nomorCode=%s&countryCode=%s", tc.nomorCode, tc.countryCode))
+func (tc *TrueCaller) FetchData() (string, string, string) {
+	url := fmt.Sprintf("https://xzhndvs.vercel.app/api/truecaller?nomorCode=%s&countryCode=%s", tc.nomorCode, tc.countryCode)
+	res, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	scanner := bufio.NewScanner(res.Body)
+	scanner.Scan()
+	jsonStr := scanner.Text()
+
+	var m map[string]interface{}
+	err = json.Unmarshal([]byte(jsonStr), &m)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	defer resp.Body.Close()
+	data := m["data"].(map[string]interface{})["data"].([]interface{})[0].(map[string]interface{})
+	name := data["name"].(string)
+	phone := data["phones"].([]interface{})[0].(map[string]interface{})
+	provider := phone["carrier"].(string)
+	nationalFormat := phone["nationalFormat"].(string)
 
-	var data Response
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		panic(err)
-	}
-
-	return &data.Data.Data[0], nil
+	return name, provider, nationalFormat
 }
 
 func main() {
+	tc := TrueCaller{}
+
+	tc.Start()
+
 	for {
-		var nomorCode string
-		var countryCode string
+		fmt.Print(color.RedString("Enter nomorCode : "))
+		fmt.Scanln(&tc.nomorCode)
+		fmt.Print(color.RedString("Enter countryCode : "))
+		fmt.Scanln(&tc.countryCode)
 
-		prompt := &survey.Input{
-			Message: "Enter nomorCode :",
-		}
-		survey.AskOne(prompt, &nomorCode)
+		name, provider, nationalFormat := tc.FetchData()
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendRow([]interface{}{"Name", name})
+		t.AppendRow([]interface{}{"Provider", provider})
+		t.AppendRow([]interface{}{"National Format", nationalFormat})
+		t.Render()
 
-		prompt = &survey.Input{
-			Message: "Enter countryCode :",
-		}
-		survey.AskOne(prompt, &countryCode)
-
-		tc := &TrueCaller{
-			nomorCode:   nomorCode,
-			countryCode: countryCode,
-		}
-
-		data, err := tc.fetchData()
-		if err != nil {
-			panic(err)
-			os.Exit(1)
-		}
-
-		name := data.Name
-		phone := data.Phones[0]
-		provider := phone.Carrier
-		nationalFormat := phone.NationalFormat
-
-		result := fmt.Sprintf("Name: %s\nProvider: %s\nNational Format: %s", name, provider, nationalFormat)
-		fmt.Println(result)
+		fmt.Println()
 	}
 }
